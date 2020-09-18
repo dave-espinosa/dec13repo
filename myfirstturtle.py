257b9ec7e9d085fe6d0d1ee19af67e831b4abddc
dave-espinosa <71471098+dave-espinosa@users.noreply.github.com>
2020-09-18 16:06:18 -0500

    Added 'myfirstturtle.py'
    The program 'myfirstturtle.py' is a brief first approach with Python 3.

---
+++ myfirstturtle.py
@@ -0,0 +1,48 @@
+import turtle
+from math import sqrt
+from random import randrange
+
+def colorgen ():
+    color_matrix = ["red", "green", 'blue', "yellow", "orange", "violet"]
+    color_choosen = color_matrix[randrange(0, len(color_matrix))]
+    return (color_choosen)
+
+wn = turtle.Screen()
+dave = turtle.Turtle()
+dave.pensize(4)
+
+
+#a = int(input("Insert Hexagon Side Dimension:"))
+a =60
+for _ in range(6):
+    dave.color(colorgen())
+    dave.forward(a)
+    dave.right(60)
+
+dave.right(30)
+b = a*sqrt(3)
+
+dave.color(colorgen())
+dave.forward(b)
+dave.right(120)
+dave.color(colorgen())
+dave.forward(b)
+dave.right(120)
+dave.color(colorgen())
+dave.forward(b)
+dave.up()
+
+dave.right(90)
+dave.color(colorgen())
+dave.forward(a)
+dave.right(90)
+dave.down()
+
+dave.color(colorgen())
+dave.forward(b)
+dave.right(120)
+dave.color(colorgen())
+dave.forward(b)
+dave.right(120)
+dave.color(colorgen())
+dave.forward(b)
